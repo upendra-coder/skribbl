92f720453b5e063560063181c86e06d390dfb672
upen_nagar <136870644+upendra-coder@users.noreply.github.com>
2025-12-03 03:05:13 +0530

    check

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // This ensures we only create ONE connection for the whole app
-const serverURL = "http://localhost:5000";
+const serverURL = "http://192.168.29.187:5000";
 const socket = io(serverURL, { transports: ["websocket"] });
 
 export default socket;
